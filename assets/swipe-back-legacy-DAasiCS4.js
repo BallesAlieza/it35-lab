0a0cc90d6139472f1f10ddb60cc4f01342450cde
BallesAlieza <aliezaballes2@gmail.com>
2025-02-17 08:42:48 +0800

    Updates

--- assets/swipe-back-legacy-DAasiCS4.js
+++ assets/swipe-back-legacy-DAasiCS4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DSQQhwgh.js","./index3-legacy-BDB_d314.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-GI1uhaDi.js","./index3-legacy-gFd1sD3i.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
