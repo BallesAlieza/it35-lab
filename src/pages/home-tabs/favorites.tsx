c2179ca3881afb0c4155c18504fbe49b6dee9b43
BallesAlieza <aliezaballes2@gmail.com>
2025-02-17 08:10:14 +0800

    added tabs and tab routing

--- src/pages/home-tabs/favorites.tsx
+++ src/pages/home-tabs/favorites.tsx
@@ -2,11 +2,18 @@ import {
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonIcon, 
+      IonLabel, 
       IonMenuButton, 
       IonPage, 
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabButton, 
+      IonTabs, 
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+  
   
   const favorites: React.FC = () => {
     return (
@@ -20,6 +27,16 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Favorites
+        </div>
         </IonContent>
       </IonPage>
     );
